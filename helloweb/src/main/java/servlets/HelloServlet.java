1b8592051ecbca91ac47c12f8cb10b23bb7cc2ef
thddb <thddb@YUNA-GB3>
2023-09-06 10:51:16 +0900

    helloservlet

--- helloweb/src/main/java/servlets/HelloServlet.java
+++ helloweb/src/main/java/servlets/HelloServlet.java
@@ -13,7 +13,7 @@ public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.setContentType("text/html; charset=uft-8");
+		response.setContentType("text/html; charset=utf-8");
 		PrintWriter pw = response.getWriter();
 		
 		pw.print("<h1>Hello World</h1>");
